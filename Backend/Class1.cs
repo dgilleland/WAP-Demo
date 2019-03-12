36e7652c3b4ffe36fdab1d1c508622238484a6ee
Dan Gilleland <dgilleland@users.noreply.github.com>
2019-03-12 10:45:38 -0600

    quick demo

---
+++ Backend/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SecurityContext : DbContext
+    {
+        public SecurityContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<SecurityRole> SecurityRoles { get; set; }
+    }
+
+    [Table("UserAccounts", Schema = "Security")]
+    public class UserAccount
+    {
+        [Key]
+        public int UserAccountID { get; set; }
+        public string WindowsAccount { get; set; }
+        public string DisplayName { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Permission> Roles { get; set; }
+        = new HashSet<Permission>();
+    }
+    [Table("Permissions", Schema = "Security")]
+    public class Permission
+    {
+        // Composite key
+        [Key, Column(Order = 1)]
+        public int UserAccountID { get; set; }
+        [Key, Column(Order = 2)]
+        public int SecurityRoleID { get; set; }
+        public DateTime? Expires { get; set; }
+
+        // Navigation Properties
+        public virtual UserAccount UserAccount { get; set; }
+        public virtual SecurityRole SecurityRole { get; set; }
+    }
+    [Table("SecurityRoles", Schema = "Security")]
+    public class SecurityRole
+    {
+        #region Fixed security roles
+        public const string SUser = nameof(SUser);
+        #endregion
+
+        [Key]
+        public int SecurityRoleID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Navigation Properties
+        public virtual ICollection<Permission> Users { get; set; }
+        = new HashSet<Permission>();
+    }
+}
---
+++ WAP/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WAP
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_OnPostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            // Get a reference to the current user
+            IPrincipal loggedInUser = HttpContext.Current.User;
+            // Check the database for any roles and add them to the user
+        }
+    }
+}
